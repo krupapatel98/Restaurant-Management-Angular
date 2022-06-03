65c1d085fb63534f3d71ce276cd8e65698ba6b1e
Krupa <krupap333@gmail.com>
2022-06-03 06:38:04 +0530

    Reactive Forms Part 1

--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { Feedback } from '../shared/feedback';
+import  { ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;
+  feedback: Feedback | undefined;
+  contactType:typeof ContactType;
+  
+  constructor(private fb:FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit(){
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
